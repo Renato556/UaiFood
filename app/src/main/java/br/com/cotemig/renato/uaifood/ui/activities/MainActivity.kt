ebc54de1138fd8a1e9caed6a3f80bd80b8a9b2df
MARCOS_GABRIEL_ALVES_CORREA <72001224@DELTAB31.BARROCA>
2022-05-25 20:08:19 -0300

    Adicionadas pastas models, services, ui

--- app/src/main/java/br/com/cotemig/renato/uaifood/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/cotemig/renato/uaifood/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.cotemig.renato.uaifood
+package br.com.cotemig.renato.uaifood.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.cotemig.renato.uaifood.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
