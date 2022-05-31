283f8bccb514d8460a3759de021140e265be4897
MARCOS_GABRIEL_ALVES_CORREA <72001224@DELTAB31.BARROCA>
2022-05-31 20:21:14 -0300

    tela forgot

---
+++ app/src/main/java/br/com/cotemig/renato/uaifood/ui/activities/ForgotActivity.kt
@@ -0,0 +1,12 @@
+package br.com.cotemig.renato.uaifood.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.cotemig.renato.uaifood.R
+
+class ForgotActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot)
+    }
+}
