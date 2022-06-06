fc83922727c3691b6af48b1201a9676b8bfb4167
correamarcos <83926140+correamarcos@users.noreply.github.com>
2022-06-06 17:13:47 -0300

    Adicionada tela detalhe do produto

---
+++ app/src/main/java/br/com/cotemig/renato/uaifood/ui/activities/DetalheProdutoActivity.kt
@@ -0,0 +1,11 @@
+package br.com.cotemig.renato.uaifood.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalheProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe_produto)
+    }
+}
