32016e20f80ea6a592556a5d246257636a81feb2
Renato <53575798+Renato556@users.noreply.github.com>
2022-06-06 21:40:42 -0300

    cor do fundo padronizada nas telas

--- app/src/main/java/br/com/cotemig/renato/uaifood/ui/activities/DetalheProdutoActivity.kt
+++ app/src/main/java/br/com/cotemig/renato/uaifood/ui/activities/DetalheProdutoActivity.kt
@@ -2,6 +2,7 @@ package br.com.cotemig.renato.uaifood.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.cotemig.renato.uaifood.R
 
 class DetalheProdutoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
